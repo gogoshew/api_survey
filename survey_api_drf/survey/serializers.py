2ebae60df8f82d081a6794e5f59ca7e42d7c2822
gogoshew <iabalymov@gmail.com>
2022-04-17 12:50:20 +0300

    serializers commit

--- survey_api_drf/survey/serializers.py
+++ survey_api_drf/survey/serializers.py
@@ -1,47 +1,32 @@
 from rest_framework import serializers
-from .models import *
-
-
-class CurrentUserDefault(object):
-    def set_context(self, serializer_field):
-        self.user_id = serializer_field.context['request'].user.id
-
-    def __call__(self):
-        return self.user_id
+from .models import Survey, Question, Answer, Choice
 
 
 class AnswerSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    user_id = serializers.IntegerField(default=CurrentUserDefault())
-    survey = serializers.SlugRelatedField(queryset=Survey.objects.all(), slug_field='id')
-    question = serializers.SlugRelatedField(queryset=Question.objects.all(), slug_field='id')
-    choice = serializers.SlugRelatedField(queryset=Choice.objects.all(), slug_field='id', allow_null=True)
-    choice_text = serializers.CharField(max_length=200, allow_null=True, required=False)
 
     class Meta:
         model = Answer
         fields = '__all__'
 
-    def create(self, validated_data):
-        return Answer.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
-        instance.save()
-        return instance
+class ChoiceSerializer(serializers.ModelSerializer):
 
-    def validate(self, attrs):
-        question_type = Question.objects.get(id=attrs['question'].id).question_type
-        try:
-            if question_type == "one" or question_type == "text":
-                obj = Answer.objects.get(question=attrs['question'].id, survey=attrs['survey'],
-                                         user_id=attrs['user_id'])
-            elif question_type == "multiple":
-                obj = Answer.objects.get(question=attrs['question'].id, survey=attrs['survey'],
-                                         user_id=attrs['user_id'],
-                                         choice=attrs['choice'])
-        except Answer.DoesNotExist:
-            return attrs
-        else:
-            raise serializers.ValidationError('Вы уже дали ответ.')
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True)
+
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class SurveySerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True)
+
+    class Meta:
+        model = Survey
+        fields = '__all__'
